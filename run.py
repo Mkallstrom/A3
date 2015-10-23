ebd7a4fe97f15dc31d9a30c0b89b29dce897c51b
Mkallstrom <martinkallstrom92@gmail.com>
2015-10-23 15:21:01 +0100

    b

--- run.py
+++ run.py
@@ -10,10 +10,11 @@ tweetnum = 19
 
 while(tweetpointer <= tweetnum):
     url = "http://smog.uppmax.uu.se:8080/swift/v1/tweets/tweets_" + str(tweetpointer) + ".txt"
-    objs = get_unique_tweets(url)
-    for word in words:
-        counts[words.index(word)] += count_word(objs, word)        
+    (words,fcounts) = get_unique_tweets(url) 
+    print(words,fcounts)
+    for c in counts:
+        c += fcounts[counts.index(c)]
     tweetpointer += 1;
-print(words,count)
+print(words,counts)
 
 print("Done running run")
--- tasks.py
+++ tasks.py
@@ -7,32 +7,30 @@ app = Celery('tasks', backend='amqp', broker='amqp://')
 
 @app.task
 def get_unique_tweets(url):
+
+    counts = [0,0,0,0,0,0,0]
+    words = ["han","hon","den","det","denna","denne","hen"]
+
     print("Now opening " + url)
     data = urllib2.urlopen(url)
     print("Url successfully opened.")
-    objs = []
     for line in data:
         while True:
             try:
                 jfile = json.loads(line)
                 if not(jfile['text'].startswith("RT")):
-                    objs.append(jfile)
+                    for word in words:
+                        counts[words.index(word)] += count_word(jfile, word) 
                 break
             except ValueError:
                 try:
                     line+= next(data)
                 except StopIteration:
-                    print("Returned from exception")
-                    return objs
+                    return (words,counts)
                     
-    print("Returned normally")
-    return objs
+    return (words,counts)
 
 @app.task
-def count_word(objs, word):
-    print("Now counting occurences of " + word)
-    count = 0
-    for obj in objs:
-        count += obj['text'].count(word)
-    print("Found " + str(count) + " occurences of " + word)
+def count_word(obj, word):
+    count = obj['text'].count(word)
     return count

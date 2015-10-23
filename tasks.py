c51cc6dc6f3d4d2e1e4bb1b1731b37a57559b8df
Mkallstrom <martinkallstrom92@gmail.com>
2015-10-23 12:44:23 +0100

    Com

--- tasks.py
+++ tasks.py
@@ -1,7 +1,45 @@
+import urllib2
+import json
+import itertools
 from celery import Celery
 
 app = Celery('tasks', backend='amqp', broker='amqp://')
 
 @app.task
-def counter():
-    return 0;
+def get_unique_tweets(data):
+    objs = []
+    for line in data:
+        while True:
+            try:
+                jfile = json.loads(line)
+                if not(jfile['text'].startswith("RT")):
+                    objs.append(jfile)
+                break
+            except ValueError:
+                line+= next(data)
+
+    return objs
+
+@app.task
+def count_word(objs, word):
+    count = 0
+    for obj in objs:
+        count += obj['text'].count(word)
+    return count
+
+@app.task
+def run():
+    counts = [0,0,0,0,0,0,0]
+    words = ["han","hon","den","det","denna","denne","hen"]
+
+    tweetpointer = 0
+    tweetnum = 19
+    while(tweetpointer <= tweetnum):
+        data = urllib2.urlopen("http://smog.uppmax.uu.se:8080/swift/v1/tweets/tweets_" + str(tweetpointer) + ".txt")
+        objs = get_unique_tweets(data)
+        for word in words:
+            counts[words.index(word)] += count_word(objs, word)
+        tweetpointer += 1;
+    return (words, counts)
+
+print(run())

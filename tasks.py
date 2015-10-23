667122106dbfc954680857188f34103b10d07dff
Mkallstrom <martinkallstrom92@gmail.com>
2015-10-23 20:25:06 +0100

    a

--- tasks.py
+++ tasks.py
@@ -6,6 +6,28 @@ from celery import Celery
 app = Celery('tasks', backend='amqp', broker='amqp://')
 
 @app.task
+def count_pronouns():
+    counts = [0,0,0,0,0,0,0]
+    words = ["han","hon","den","det","denna","denne","hen"]
+
+    tweetpointer = 0
+    tweetnum = 19
+
+    while(tweetpointer <= tweetnum):
+        url = "http://smog.uppmax.uu.se:8080/swift/v1/tweets/tweets_" + str(tweetpointer) + ".txt"
+        (words,fcounts) = get_unique_tweets(url) 
+        print(words,fcounts)
+        for c in counts:
+            c += fcounts[counts.index(c)]
+        tweetpointer += 1;
+    print(words,counts)
+    data = {}
+    for word in words:
+        data[word] = count[words.index(word)]
+    json_data = json.dumps(data)
+    return(json_data)
+
+@app.task
 def get_unique_tweets(url):
 
     counts = [0,0,0,0,0,0,0]

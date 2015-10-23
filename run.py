5f189cc2bd201e98e9e6e3996dc50c6f4a0c4545
Mkallstrom <martinkallstrom92@gmail.com>
2015-10-23 14:58:16 +0100

    a

--- run.py
+++ run.py
@@ -1,5 +1,19 @@
-from tasks import run
+from tasks import get_unique_tweets
+from tasks import count_word
 
-print("Running run()")
-run()
-print("Done running run()")
+print("Running run")
+counts = [0,0,0,0,0,0,0]
+words = ["han","hon","den","det","denna","denne","hen"]
+
+tweetpointer = 0
+tweetnum = 19
+
+while(tweetpointer <= tweetnum):
+    url = "http://smog.uppmax.uu.se:8080/swift/v1/tweets/tweets_" + str(tweetpointer) + ".txt"
+    objs = get_unique_tweets(url)
+    for word in words:
+        counts[words.index(word)] += count_word(objs, word)
+        tweetpointer += 1;
+print(words,count)
+
+print("Done running run")
--- tasks.py
+++ tasks.py
@@ -6,8 +6,9 @@ from celery import Celery
 app = Celery('tasks', backend='amqp', broker='amqp://')
 
 @app.task
-def get_unique_tweets(data):
-    print("Now getting unique tweets")
+def get_unique_tweets(url):
+    print("Now opening " + url)
+    data = urllib2.urlopen(url)
     objs = []
     for line in data:
         while True:
@@ -34,20 +35,3 @@ def count_word(objs, word):
         count += obj['text'].count(word)
     print("Found " + str(count) + " occurences of " + word)
     return count
-
-@app.task
-def run():
-    counts = [0,0,0,0,0,0,0]
-    words = ["han","hon","den","det","denna","denne","hen"]
-
-    tweetpointer = 0
-    tweetnum = 19
-    while(tweetpointer <= tweetnum):
-        url = "http://smog.uppmax.uu.se:8080/swift/v1/tweets/tweets_" + str(tweetpointer) + ".txt"
-        print("Now opening " + url)
-        data = urllib2.urlopen(url)
-        objs = get_unique_tweets(data)
-        for word in words:
-            counts[words.index(word)] += count_word(objs, word)
-        tweetpointer += 1;
-    return (words, counts)

5214bd6ad10ac0d57fd701081b347b2c1c854773
Mkallstrom <martinkallstrom92@gmail.com>
2015-10-24 09:33:52 +0100

    a

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def index():
     return render_template('home.html')
 
-@app.route('/count', methods=['GET'])
+@app.route('/count', methods=['POST'])
 def count():
     return render_template('count.html', count_pronouns())
 

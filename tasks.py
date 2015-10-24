f18c4ef4762299934f26e8260562752468529604
Mkallstrom <martinkallstrom92@gmail.com>
2015-10-24 10:43:49 +0100

    ads

--- tasks.py
+++ tasks.py
@@ -21,7 +21,7 @@ def count_pronouns():
         tweetpointer += 1;
     data = {}
     for word in words:
-        print(counts[words.index(word)]
+        print(counts[words.index(word)])
         data[word] = counts[words.index(word)]
     print(data)
     json_data = json.dumps(data)

1d3b657adf00ea3c4d41a2df398bf752eeac6118
Mkallstrom <martinkallstrom92@gmail.com>
2015-10-24 09:15:33 +0100

    x

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def index():
     return render_template('home.html')
 
-@app.route('/count', methods=['POST'])
+@app.route('/count', methods=['GET'])
 def count():
     return render_template('count.html', count_pronouns())
 
--- tasks.py
+++ tasks.py
@@ -11,7 +11,7 @@ def count_pronouns():
     words = ["han","hon","den","det","denna","denne","hen"]
 
     tweetpointer = 0
-    tweetnum = 19
+    tweetnum = 0
 
     while(tweetpointer <= tweetnum):
         url = "http://smog.uppmax.uu.se:8080/swift/v1/tweets/tweets_" + str(tweetpointer) + ".txt"

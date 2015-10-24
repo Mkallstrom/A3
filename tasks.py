ee41b0384c746c31e5701101872a2f66537721d4
Mkallstrom <martinkallstrom92@gmail.com>
2015-10-24 10:52:52 +0100

    ads

--- tasks.py
+++ tasks.py
@@ -18,7 +18,8 @@ def count_pronouns():
         url = "http://smog.uppmax.uu.se:8080/swift/v1/tweets/tweets_" + str(tweetpointer) + ".txt"
         (words,fcounts) = get_unique_tweets(url) 
         for c in fcounts:
-            data[words[fcounts.index(c)]] = str(int(data[words.index(c)]) + c)
+            word = words[fcounts.index(c)]
+            data[word] = str(int(data[word]) + c)
         tweetpointer += 1;
     json_data = json.dumps(data)
     return(json_data)
